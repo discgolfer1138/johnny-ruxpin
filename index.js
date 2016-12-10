7d8d962df05fe82dbb71aa310c8c7abdb19cbfa2
Chad Francis <chad.francis@mapquest.com>
2016-12-10 01:47:06 -0700

    added basic motor sample

---
+++ index.js
@@ -0,0 +1,32 @@
+var five = require('johnny-five');
+var Chip = require('chip-io');
+
+var board = new five.Board({
+  io: new Chip()
+});
+
+board.on('ready', function() {
+  console.log('board ready', Date.now());
+
+  var motor = new five.Motor(['XIO-P0', 'XIO-P2']);
+
+  motor.on('forward', function() {
+    console.log('forward', Date.now());
+
+    // demonstrate switching to reverse after 5 seconds
+    board.wait(5000, function() {
+      motor.reverse(50);
+    });
+  });
+
+  motor.on('reverse', function() {
+    console.log('reverse', Date.now());
+
+    // demonstrate stopping after 5 seconds
+    board.wait(5000, function() {
+      motor.stop();
+    });
+  });
+
+  motor.forward(50);
+});
