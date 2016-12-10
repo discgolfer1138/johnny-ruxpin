c1a0b1e15527ef7c3d780a97ebbb5dee0f5e5ba0
Chad Francis <chad.francis@mapquest.com>
2016-12-10 02:14:16 -0700

    more pin crap

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ var board = new five.Board({
 board.on('ready', function() {
   console.log('board ready', Date.now());
 
-  var motor = new five.Motor(['PWM0', 'XIO-P0', 'XIO-P2']);
+  var motor = new five.Motor({
+    pwm:'PWM0', 
+    cdir: 'XIO-P0', 
+    dir: 'XIO-P2'
+  });
 
   motor.on('forward', function() {
     console.log('forward', Date.now());
