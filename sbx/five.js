47f416f8f3e8092e143c8b04ffe2870af2a89513
Chad Francis <yamanote1138@gmail.com>
2017-11-24 20:41:15 +0000

    added sandbox files, updated package.json

---
+++ sbx/five.js
@@ -0,0 +1,10 @@
+var five = require('johnny-five');
+var chipio = require('chip-io');
+
+var board = new five.Board({
+  io: new chipio()
+});
+
+board.on('ready', function() {
+  // do Johnny-Five stuff
+});
