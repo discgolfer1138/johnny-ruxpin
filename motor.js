b29b8afc05cf93fbe70e64878025f5444724f7b6
Chad Francis <yamanote1138@gmail.com>
2020-01-02 00:52:06 -0700

    removing timing test

--- motor.js
+++ motor.js
@@ -17,13 +17,13 @@ board.on('ready', function() {
   // let BIN1 = 'GPIO22'; //15
 
   const eyes =  new Motor([PWMA, AIN2, AIN1]);
-  eyes.on('forward', function(err) {
-    console.log(err);
-    // demonstrate braking after 5 seconds
-    board.wait(3000, function() {
-      eyes.stop();
-    });
-  });
+  // eyes.on('forward', function(err) {
+  //   console.log(err);
+  //   // demonstrate braking after 5 seconds
+  //   board.wait(3000, function() {
+  //     eyes.stop();
+  //   });
+  // });
 
   eyes.stop().forward(200);
 });
