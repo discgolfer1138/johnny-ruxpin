dbcf2d05aed485e63512cf4c041a563967084b2b
Chad Francis <yamanote1138@gmail.com>
2020-01-02 01:33:29 -0700

    trying to interrupt standby

--- motor.js
+++ motor.js
@@ -1,4 +1,4 @@
-const {Board, Motor} = require('johnny-five');
+const {Board, Motor, Pin} = require('johnny-five');
 const Raspi = require('raspi-io').RaspiIO;
 
 var board = new Board({
@@ -10,11 +10,13 @@ board.on('ready', function() {
   let AIN2 = 'GPIO23'; //16
   let AIN1 = 'GPIO24'; //18
 
-  // let STBY = 'GPIO25'; //22
+  let STBY = 'GPIO25'; //22
 
   // let PWMB = 'GPIO17'; //11
   // let BIN2 = 'GPIO27'; //13
   // let BIN1 = 'GPIO22'; //15
+
+  const standby_pin = new Pin(STBY);
 
   const eyes =  new Motor({
     pins: {
@@ -24,9 +26,11 @@ board.on('ready', function() {
     }
   });
 
-  board.repl.inject({
-    eyes
+  board.on('exit', () => {
+    standby_pin.low();
   });
+
+  standby_pin.high();
 
   // Go forward at full speed for 5 seconds
   console.log('Full speed ahead!');
