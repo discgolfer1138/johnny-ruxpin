ce4a8dfeed83b8ad3286807d5b1dea11308dea06
Chad Francis <yamanote1138@gmail.com>
2020-01-02 00:42:18 -0700

    giving the new wiring a whirl

--- motor.js
+++ motor.js
@@ -6,7 +6,24 @@ var board = new Board({
 });
 
 board.on('ready', function() {
-  // const motor = new Motor([ "a5", "a4", "a3" ]);
-  var led = new board.Led('P1-13');
-  led.blink();
+  let PWMA = 12;
+  let AIN2 = 16;
+  let AIN1 = 18;
+
+  // STBY = 22
+
+  // PWMB = 11
+  // BIN2 = 13
+  // BIN1 = 15
+
+  const eyes =  new Motor([PWMA, AIN2, AIN1]);
+  eyes.on('forward', function(err) {
+    console.log(err);
+    // demonstrate braking after 5 seconds
+    board.wait(3000, function() {
+      eyes.stop();
+    });
+  });
+
+  eyes.stop().forward(100);
 });
